2b5d10c63f3c9b743130aa2e188912cf62b8877b
ingoleritu123 <jitcse.reetaingole@gmail.com>
2023-11-07 20:21:41 +0530

    program uploaded

---
+++ OOPS C#/calExbymethodoverlodwithdiffpara.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calExinmethodoverlodwithdiffpara
+{
+    class Calculate
+    {
+        public void addition(int num1,int num2)
+        {
+            int res = num1 + num2;
+            Console.WriteLine("Addition of int is:" + res);8
+
+
+        }
+        public void addition(float num1,float num2)
+        {
+            float res = num1 + num2;
+            Console.WriteLine("Addition of float is:" + res);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calc = new Calculate();
+            calc.addition(1, 2);
+            Console.WriteLine("disply res of float");
+            calc.addition(1.5f, 3.6f);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOPS C#/distroctorprogram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distroctor_program
+{
+    class Employee
+    {
+        public Employee()
+        {
+            Console.WriteLine("call constructor");
+
+        }
+        ~Employee()
+        {
+            Console.WriteLine("call distructor");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            Console.ReadKey();
+
+            Employee emp1 = new Employee();
+            Console.ReadKey();
+        }
+    }
+}
