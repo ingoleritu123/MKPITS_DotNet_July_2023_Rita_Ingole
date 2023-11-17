8ad5c66d617e3c545e062689114c13bbf0260235
ingoleritu123 <jitcse.reetaingole@gmail.com>
2023-11-17 20:43:00 +0530

    program uploaded

---
+++ OOPS C#/Exmultilavelinheritance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exmultilavelinheritance
+{
+    public class Animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("eating");
+        }
+    }
+    public class Dog : Animal
+    {
+        public void bark()
+        {
+            Console.WriteLine("barking");
+        }
+    }
+    public class BabyDog : Dog
+    {
+        public void weep()
+        {
+            Console.WriteLine("weeping");
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            BabyDog d1 = new BabyDog();
+            d1.eat();
+            d1.bark();
+            d1.weep();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ OOPS C#/multilavelinheritance_basedclasspersonEx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multilavelinheritance_Ex_basedclassperson
+{
+    public class Person
+    {
+        string Name;
+        string Address;
+        public void getPersondata(string Name,string Address)
+        {
+            this.Name = Name;
+            this.Address = Address;
+        }
+        public void displayPersondata()
+        {
+            Console.WriteLine("Name:"+Name);
+            Console.WriteLine("Address:" + Address);
+        }
+    }
+    public class Employee:Person
+    {
+        int Emp_Id;
+        int Salary;
+        public void getEmployeedata(int Emp_Id,int Salary)
+        {
+            this.Emp_Id = Emp_Id;
+            this.Salary = Salary;
+        }
+        public void displayEmployeedata()
+        {
+            Console.WriteLine("Emp_Id:" + Emp_Id);
+            Console.WriteLine("Salary:" + Salary);
+        }
+        
+    }
+    public class Parttime_Emp:Employee
+    {
+        int no_of_hours;
+        public void getParttimeEmpdata(int no_of_hours)
+        {
+            this.no_of_hours = no_of_hours;
+        }
+        public void displayParttimeEmpdata()
+        {
+            Console.WriteLine("No_Of_Hours:" + no_of_hours);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Parttime_Emp p1 = new Parttime_Emp();
+            p1.getPersondata("Rita", "nagpur");
+            p1.getEmployeedata(345, 67000);
+            
+            p1.getParttimeEmpdata(5);
+            p1.displayPersondata();
+            p1.displayEmployeedata();
+            p1.displayParttimeEmpdata();
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
