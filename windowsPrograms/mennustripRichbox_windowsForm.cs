efab0f4b3785df6b0cfc91eb025f8b128fe8e7a0
ingoleritu123 <jitcse.reetaingole@gmail.com>
2023-12-07 17:49:41 +0530

    program uploaded

---
+++ windowsPrograms/mennustripRichbox_windowsForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Menustrip_richtextbox_windowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog()==DialogResult.OK)
+            {
+                richTextBox1.ForeColor = colorDialog1.Color;
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(fontDialog1.ShowDialog()==DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fontDialog1.Font;
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                string fn = openFileDialog1.FileName;
+
+                MessageBox.Show(fn);
+                if(fn.EndsWith("rtf"))
+                {
+                    richTextBox1.LoadFile(openFileDialog1.FileName);
+                }
+                else
+                {
+                    MessageBox.Show("file cannot be open");
+                }
+
+            }
+        }
+
+        private void saveToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                string fn = saveFileDialog1.FileName;
+                MessageBox.Show(fn);
+                if(fn.EndsWith("rtf"))
+                {
+                    richTextBox1.SaveFile(saveFileDialog1.FileName);
+                    MessageBox.Show("Save Successfully.");
+
+                }
+                else
+                {
+                    MessageBox.Show("cannot save  file of this format");
+                }
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            richTextBox1.Width = hScrollBar1.Value;
+        }
+    }
+}
---
+++ windowsPrograms/multiplechoiceQ_windowsForms.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiplechoiceQuestion_windowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void multipleChoiceQuestionPaperToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MultipleChoiceQuestionPaper p = new MultipleChoiceQuestionPaper();
+            p.MdiParent = this;
+            p.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void multipleChoiceQuestionPaperToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+            MultipleChoiceQuestionPaper p = new MultipleChoiceQuestionPaper();
+            p.MdiParent = this;
+            p.Show();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="admin" && textBox2.Text=="admin")
+            {
+                MessageBox.Show("login Successful");
+                multipleChoiceQuestionPaperToolStripMenuItem.Enabled = true;
+                panel1.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Credentials.");
+
+               
+            }
+
+        }
+    }
+}
