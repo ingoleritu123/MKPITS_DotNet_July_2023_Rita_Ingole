e48512fe52d7aef455497597bafe59a1fe016cd7
ingoleritu123 <jitcse.reetaingole@gmail.com>
2023-12-08 21:33:09 +0530

    program upoladed

---
+++ windowsPrograms/Cafemanagmentsystem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CafeManagmentSystem_windowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="admin" && textBox2.Text=="admin")
+            {
+                MessageBox.Show("login successfull!!");
+                ordersToolStripMenuItem.Enabled = true;
+                panel1.Hide();
+
+            }
+            else
+            {
+                MessageBox.Show("Oops Invalid Crediantials.");
+            }
+        }
+    }
+}
