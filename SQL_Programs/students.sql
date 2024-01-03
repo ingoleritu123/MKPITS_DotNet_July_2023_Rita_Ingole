b78745fdcb93df5a10b70655d59035defde9e53a
ingoleritu123 <jitcse.reetaingole@gmail.com>
2024-01-03 16:08:59 +0530

    program uploaded

---
+++ SQL_Programs/students.sql
@@ -0,0 +1,3 @@
+create table students( rollno int primary key,name varchar(100) not null,gender varchar(50) not null,hobby varchar(60)not null,city varchar(50),dob varchar(50)not null)
+
+select * from students
