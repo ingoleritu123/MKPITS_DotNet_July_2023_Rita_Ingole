d9915f4c3ed05c6558f27af3c92bc6196a6e51d4
ingoleritu123 <jitcse.reetaingole@gmail.com>
2024-01-11 17:11:12 +0530

    program uploaded

---
+++ SQL_Programs/users1.sql
@@ -0,0 +1,10 @@
+create table users(username varchar(100),password varchar(100)not null)
+
+insert into users values('ritu ingole','ritu@12345')
+insert into users values('dikshant ingole','dikshu@12345')
+insert into users values('hira ingole','hitu@12345')
+insert into users values('anurag wankhede','ritu@12345')
+
+select * from users
+
+
